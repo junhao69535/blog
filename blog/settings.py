abb3f6c32bbed8408f18f8f957686dad1a63b905
junhao69535 <38750962+junhao69535@users.noreply.github.com>
2018-07-17 18:13:58 +0800

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -79,12 +79,8 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'myblog',
-        'USER':'root',
-        'PASSWORD':'123456',
-        'HOST':'localhost',
-        'PORT':'3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
     }
 }
 
